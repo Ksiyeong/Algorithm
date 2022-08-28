82372589e1ae641c07d8ece23be5d2d8c9c4b6ff
Ksiyeong <yuiop2767@naver.com>
2022-08-28 22:21:29 +0900

    CodeUp1013

--- CodeUp/src/Main1013.java
+++ CodeUp/src/Main1013.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main1013 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
+        String input = sc.nextLine();
         String[] result = input.split(" ");
         int n1 = Integer.parseInt(result[0]);
         int n2 = Integer.parseInt(result[1]);
