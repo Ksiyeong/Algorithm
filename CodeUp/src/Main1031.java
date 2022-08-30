13bf81bbee2a40a8c74a50bce9cfa73b6b405674
Ksiyeong <yuiop2767@naver.com>
2022-08-30 22:57:48 +0900

    codeup1031

---
+++ CodeUp/src/Main1031.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main1031 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(Integer.toString(i, 8)); /*입력값, n진수*/
+    }
+}
