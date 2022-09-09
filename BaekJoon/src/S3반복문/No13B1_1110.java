93372cc06f7192df44191f53586f2b5232fd079a
Ksiyeong <yuiop2767@naver.com>
2022-09-09 17:43:44 +0900

    ~ BaekJoon 반복문

--- BaekJoon/src/S3반복문/No13B1_1110.java
+++ BaekJoon/src/S3반복문/No13B1_1110.java
@@ -6,24 +6,17 @@ public class No13B1_1110 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        int subN = N; // 비교를 위한 N 맨 처음 비교때문에 필요
+        int newN = N; // 새롭게 생성될 N
         int addN; // N의 각 자릿수를 더한 값
-        int N1; // N, subN, newN의 1의 자리수
-        int newN = -1; // 새롭게 생성된 N 디폴트값으로 -1을 준 이유는 N이 0일 때를 위해
+        int N1; // N(newN)의 1의 자리수
         int count = 0;
 
-        while (N != newN) {
-            if (subN < 10) {
-                N1 = subN;
-                addN = subN;
-            } else {
-                N1 = subN % 10;
-                addN = subN / 10 + N1;
-            }
+        do {
+            N1 = newN % 10;
+            addN = newN / 10 + N1;
             newN = N1 * 10 + addN % 10;
-            subN = newN;
             count += 1;
-        }
+        } while (N != newN);
 
         System.out.println(count);
     }
