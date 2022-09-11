a96f7f95d407c7fa340cf20c426fd3fa4c41630e
Ksiyeong <yuiop2767@naver.com>
2022-09-11 17:59:32 +0900

    ~ BaekJoon 일차원배열

---
+++ BaekJoon/src/S4일차원배열/No3B2_3052.java
@@ -0,0 +1,19 @@
+package S4일차원배열;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class No3B2_3052 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        HashSet<Integer> arrSet = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            arrSet.add(sc.nextInt() % 42);
+        }
+
+
+        System.out.println(arrSet.size());
+    }
+}
---
+++ BaekJoon/src/S4일차원배열/No43B1_1546.java
@@ -0,0 +1,27 @@
+package S4일차원배열;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class No43B1_1546 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        double[] result = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            result[i] = sc.nextInt();
+        }
+        Arrays.sort(result);
+
+        double newResult = 0;
+
+        for (double i : result) {
+            newResult += i / result[n - 1] * 100;
+        }
+
+        System.out.println(newResult / n);
+    }
+}
