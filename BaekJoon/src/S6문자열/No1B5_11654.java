d175377953894a994ccf95b2a68d8eaeba119f8c
Ksiyeong <yuiop2767@naver.com>
2022-09-15 22:23:10 +0900

    ~ BaekJoon 문자열

---
+++ BaekJoon/src/S6문자열/No1B5_11654.java
@@ -0,0 +1,12 @@
+package S6문자열;
+
+import java.util.Scanner;
+
+public class No1B5_11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+
+        System.out.println(c+0);
+    }
+}
