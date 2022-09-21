f636f6dbd8c30861ed16ca3df93735bc33e9c98a
Ksiyeong <yuiop2767@naver.com>
2022-09-21 18:54:41 +0900

    ~ BaekJoon 기본수학1 달팽이

---
+++ BaekJoon/src/S7기본수학1/No4S5_2869.java
@@ -0,0 +1,26 @@
+package S7기본수학1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No4S5_2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int day = (V - B) / (A - B);
+
+
+        if ((V - B) % (A - B) != 0) {
+            day += 1;
+        }
+
+        System.out.println(day);
+    }
+}
