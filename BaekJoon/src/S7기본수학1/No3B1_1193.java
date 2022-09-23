ca342d8092d8ab9b6aeb1d406754b9c68e559599
Ksiyeong <yuiop2767@naver.com>
2022-09-23 18:39:15 +0900

    ~ BaekJoon 기본수학1 분수찾기

--- BaekJoon/src/S7기본수학1/No3B1_1193.java
+++ BaekJoon/src/S7기본수학1/No3B1_1193.java
@@ -7,28 +7,29 @@ public class No3B1_1193 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        int limit = 1;
-        int size = 1;
+        int limit = 1; // N값이 포함되어 있는 위치 중 최대값
+        int size = 1; // limit 위치의 분자(분모)의 값 중 큰 수
 
-        while (limit < N) {
+        // 1 2 3 4 5 6 순으로 최대값이 커짐
+        while (limit < N) { // N값의 위치 중 최대값을 뽑아낸다
             size++;
             limit += size;
         }
 
-        int n;
-        int d;
+        int n; // 분자
+        int d; // 분모
 
-        if (size % 2 == 0) {
-            n = size;
+        if (size % 2 == 0) { // size가 짝수일경우
+            n = size; // 분자가 큰수이다
             d = 1;
-            for (int i = 0; i < limit - N; i++) {
+            for (int i = 0; i < limit - N; i++) { // 최대값과 N값의 차이만큼 반복
                 n -= 1;
                 d += 1;
             }
-        } else {
+        } else { // size가 홀수일경우
             n = 1;
-            d = size;
-            for (int i = 0; i < limit - N; i++) {
+            d = size; // 분모가 큰수이다
+            for (int i = 0; i < limit - N; i++) { // 최대값과 N값의 차이만큼 반복
                 n += 1;
                 d -= 1;
             }
