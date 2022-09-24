39bfaa35b46e5ba4994579b0b96fdf18a21bfa12
Ksiyeong <yuiop2767@naver.com>
2022-09-24 21:09:13 +0900

    ~ BaekJoon 기본수학2 소수 찾기

---
+++ BaekJoon/src/S8기본수학2/No1S5_1978.java
@@ -0,0 +1,27 @@
+package S8기본수학2;
+
+import java.util.Scanner;
+
+public class No1S5_1978 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.nextLine());
+        String[] nums = sc.nextLine().split(" ");
+
+        int result = N;
+
+        for (String num : nums) {
+            int inum = Integer.parseInt(num);
+            result -= inum == 1 ? 1 : 0;
+
+            for (int j = 2; j < inum; j++) {
+                if (inum % j == 0) {
+                    result--;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
