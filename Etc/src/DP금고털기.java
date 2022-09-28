b319620ecd3d929bf38666f0196f40d3c47cf2f7
Ksiyeong <yuiop2767@naver.com>
2022-09-28 16:03:25 +0900

    ~ Etc DP금고털기(동전구하기)

---
+++ Etc/src/DP금고털기.java
@@ -0,0 +1,38 @@
+public class DP금고털기 {
+    public static void main(String[] args) {
+        long output = ocean(50, new int[]{10, 20, 50});
+        System.out.println(output); // 4
+
+        output = ocean(100, new int[]{10, 20, 50});
+        System.out.println(output); // 10
+
+        output = ocean(30, new int[]{5, 6, 7});
+        System.out.println(output); // 4
+    }
+
+    public static long ocean(int target, int[] type) {
+        // TODO: 다이나믹 프로그래밍을 사용해보자
+        // 동전 구하기 알고리즘
+        // target=50 {10, 20, 50}
+        //     0 10 20 30 40 50
+        //  0  1  0  0  0  0  0
+        // 10  1  1  1  1  1  1
+        // 20  1  1  2  2  3  3
+        // 50  1  1  2  2  3  4
+
+        // 화폐단위만큼+1, target+1만큼 -> 0일 경우를 생성
+        long[][] ways = new long[type.length + 1][target + 1];
+            ways[0][0] = 1;
+
+        for (int i = 1; i < ways.length; i++) {
+            for (int j = 0; j < target + 1; j++) {
+                if (j - type[i-1] >= 0) {
+                    ways[i][j] = ways[i - 1][j] + ways[i][j - type[i-1]];
+                } else {
+                    ways[i][j] = ways[i - 1][j];
+                }
+            }
+        }
+        return ways[type.length][target];
+    }
+}
