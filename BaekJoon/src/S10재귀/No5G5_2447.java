1aa93d49f14b463a7e59f2e4026002d25240abb5
Ksiyeong <yuiop2767@naver.com>
2022-10-15 23:31:21 +0900

    ~ BaekJoon 재귀 - 별 찍기

---
+++ BaekJoon/src/S10재귀/No5G5_2447.java
@@ -0,0 +1,15 @@
+package S10재귀;
+
+import java.util.Scanner;
+
+public class No5G5_2447 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+    }
+
+    static String star(int N) {
+        return "";
+    }
+}
