4021d10ca297a43248fe61cfa187a3a514e4f956
Ksiyeong <yuiop2767@naver.com>
2022-10-17 10:05:34 +0900

    ~ Etc 암호 복호화

--- Etc/src/DecryptCaesarCipher.java
+++ Etc/src/DecryptCaesarCipher.java
@@ -1,6 +1,6 @@
 public class DecryptCaesarCipher {
     public static void main(String[] args) {
-        String output = decryptCaesarCipher("khoor", 3);
+        String output = decryptCaesarCipher("nzop delepd dfaazced jzf", 11);
         System.out.println(output); // --> hello
 
         output = decryptCaesarCipher("zruog", 3);
@@ -10,11 +10,24 @@ public class DecryptCaesarCipher {
     static public String decryptCaesarCipher(String str, int secret) {
         // TODO:
         String result = "";
+        int c;
 
         for (int i = 0; i < str.length(); i++) {
-            result += String.format("%c", str.charAt(i) - secret);
+            if (str.charAt(i) == ' ') {
+                result += " ";
+            } else {
+                c = str.charAt(i) - secret;
+
+                if (c < 97) {
+                    c += 26;
+                }
+                result += String.format("%c", c);
+            }
         }
 
         return result;
     }
 }
+
+// 97
+// 122
