f332f6544365550aabc16fcc0fd6702fd225c63e
Ksiyeong <yuiop2767@naver.com>
2022-10-20 10:47:53 +0900

    ~ Etc LargestProductOfThree

--- Etc/src/LargestProductOfThree.java
+++ Etc/src/LargestProductOfThree.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 
-public class LargestProductOfThree
-{
+public class LargestProductOfThree {
     public static void main(String[] args) {
         int output = largestProductOfThree(new int[]{2, 1, 3, 7});
         System.out.println(output); // --> 42 (= 2 * 3 * 7)
@@ -20,24 +19,6 @@ public class LargestProductOfThree
         // 음수가 포함될 수 도 있기 때문에 고려해야함
         // 0이 더 클 수 도 있음
         Arrays.sort(arr);
-
-        int postiveCount = 0;
-        int zero = 0;
-        int negativeCount = 0;
-
-        for (int num : arr) {
-            if (num > 0) {
-                postiveCount += 1;
-            } else if (num == 0) {
-                zero += 1;
-            } else {
-                negativeCount += 1;
-            }
-        }
-
-        /**양수를 만들 수 있는 경우가 가장 크다
-         * 양수만 곱한 경우
-         * 양수 x 음수 x 음수*/
 
         return Math.max(arr[arr.length - 1] * arr[arr.length - 2] * arr[arr.length - 3], arr[arr.length - 1] * arr[1] * arr[0]);
     }
