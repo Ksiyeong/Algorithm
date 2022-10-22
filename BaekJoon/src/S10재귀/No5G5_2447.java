803bcbbc888527884ec205d0be9769b8e63c7467
Ksiyeong <yuiop2767@naver.com>
2022-10-22 17:35:53 +0900

    ~ BaekJoon 재귀 - 별 찍기 완성

--- BaekJoon/src/S10재귀/No5G5_2447.java
+++ BaekJoon/src/S10재귀/No5G5_2447.java
@@ -7,14 +7,18 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class No5G5_2447 {
+    static int[][] starArr;
+    static int N;
+
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         BufferedWriter bf = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = sc.nextInt();
+        N = sc.nextInt();
+        starArr = new int[N][N];
+        star(N);
 
-        int[][] resultArr = star(N);
-        for (int[] arr : resultArr) {
+        for (int[] arr : starArr) {
             String result = Arrays.toString(arr)
                     .replaceAll("\\D", "")
                     .replaceAll("1", " ")
@@ -22,53 +26,32 @@ public class No5G5_2447 {
 
             bf.write(result + "\n");
         }
-
         bf.flush();
     }
 
-    /**채워야할 수 만큼 공백으로 채운다
-     * 나머지는 출력할 때 null값을 *로 바꿔 출력해준다 ? */
-    static int[][] star(int N) {
-        int[][] starArr = new int[N][N];
+    static void star(int num) {
+        // 더 이상 쪼갤 수 없는 경우
+        if (num == 1) return;
 
-        for (int i = N/3; i < N/3*2; i++) {
-            for (int j = N/3; j < N/3*2; j++) {
-                starArr[i][j] = 1;
+        // 별을 찍을 갯수, num/3이 많이 나와서 n으로 통합
+        int n = num / 3;
+
+        for (int i = n; i < N; i += num) { // i 세로 방향의 별찍기 시작점
+
+            for (int j = n; j < N; j += num) { // j 가로 방향의 별찍기 시작점
+
+                for (int l = 0; l < n; l++) { // 세로 방향으로 찍을 별의 좌표 보정
+
+                    for (int k = 0; k < n; k++) { // 가로 방향으로 찍을 별의 좌표 보정
+
+                        starArr[i + l][j + k] = 1;
+
+                    }
+                }
             }
         }
 
+        star(n);
 
-        return starArr;
     }
 }
-/**
- *
-***************************
-* ** ** ** ** ** ** ** ** *
-***************************
-***   ******   ******   ***
-* *   * ** *   * ** *   * *
-***   ******   ******   ***
-***************************
-* ** ** ** ** ** ** ** ** *
-***************************
-*********         *********
-* ** ** *         * ** ** *
-*********         *********
-***   ***         ***   ***
-* *   * *         * *   * *
-***   ***         ***   ***
-*********         *********
-* ** ** *         * ** ** *
-*********         *********
-***************************
-* ** ** ** ** ** ** ** ** *
-***************************
-***   ******   ******   ***
-* *   * ** *   * ** *   * *
-***   ******   ******   ***
-***************************
-* ** ** ** ** ** ** ** ** *
-***************************
- *
- * */
