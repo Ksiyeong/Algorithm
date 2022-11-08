06d6e0125c1a50823959c77da75caf42610ce6d8
Ksiyeong <yuiop2767@naver.com>
2022-11-08 11:40:25 +0900

    Etc - RotateMatrix

--- Etc/src/RotateMatrix.java
+++ Etc/src/RotateMatrix.java
@@ -3,32 +3,23 @@ import java.util.Arrays;
 public class RotateMatrix {
     public static void main(String[] args) {
         int[][] matrix = new int[][]{
-                {1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12},
-                {13, 14, 15, 16}
+                {1, 2, 3},
+                {5, 6, 7},
+                {9, 10, 11},
+                {13, 14, 15}
         };
-        int K = 0;
-        System.out.println(matrix);
+        int K = 1;
         System.out.println(matrix[0][0]); // --> 1
-        System.out.println(matrix[3][2]); // --> 15
+        System.out.println(matrix[2][2]); // --> 11
 
         int[][] rotatedMatrix = rotateMatrix(matrix, K);
-        int[][] rotatedMatrix2 = rotateMatrix2(matrix, K);
 
-        System.out.printf("내가만든 배열 : %s\n",rotatedMatrix);
+        System.out.printf("내가만든 배열 : %s\n", rotatedMatrix);
         Arrays.stream(rotatedMatrix)
                 .forEach(arr -> System.out.println(Arrays.toString(arr)));
 
-        System.out.printf("레퍼런스 코드배열 : %s\n",rotatedMatrix2);
-        Arrays.stream(rotatedMatrix2)
-                .forEach(arr -> System.out.println(Arrays.toString(arr)));
-
         System.out.println(rotatedMatrix[0][0]); // --> 13
-        System.out.println(rotatedMatrix[3][2]); // --> 8
-
-        System.out.println(rotateMatrix(matrix, K)[0][0]);
-        System.out.println(rotateMatrix(matrix, K)[3][2]);
+        System.out.println(rotatedMatrix[2][2]); // --> 7
     }
 
     public static int[][] rotateMatrix(int[][] matrix, int K) {
@@ -36,9 +27,10 @@ public class RotateMatrix {
         if (matrix == null) return null;
         if (K == 0 || K % 4 == 0) return matrix;
 
-        int[][] rotatedMatrix = new int[matrix.length][matrix.length];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
+        int[][] rotatedMatrix = new int[matrix[0].length][matrix.length];
+
+        for (int i = 0; i < rotatedMatrix.length; i++) {
+            for (int j = 0; j < rotatedMatrix[0].length; j++) {
                 rotatedMatrix[i][j] = matrix[matrix.length - j - 1][i];
 
             }
@@ -47,29 +39,4 @@ public class RotateMatrix {
         return rotateMatrix(rotatedMatrix, K - 1);
     }
 
-    static public int[][] rotateMatrix2(int[][] matrix, int K) {
-        if (matrix == null || matrix.length == 0) return matrix;
-
-        int N = matrix.length;
-        int M = matrix[0].length;
-
-        int rotateNum = K % 4;
-        // 회전하지 않는다.
-        if (rotateNum == 0) return matrix;
-        // 홀수번 회전 시 M x N, 짝수번 회전 시 N x M
-        int[][] rotated = rotateNum % 2 == 1 ? new int[M][N] : new int[N][M];
-
-        for (int row = 0; row < rotated.length; row++) {
-            for (int col = 0; col < rotated[row].length; col++) {
-                if (rotateNum == 1) {
-                    rotated[row][col] = matrix[N - col - 1][row];
-                } else if (rotateNum == 2) {
-                    rotated[row][col] = matrix[N - row - 1][M - col - 1];
-                } else {
-                    rotated[row][col] = matrix[col][M - row - 1];
-                }
-            }
-        }
-        return rotated;
-    }
 }
