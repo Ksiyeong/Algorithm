81331e1966f71aa800965d875cfd61b5d024e8f5
Ksiyeong <yuiop2767@naver.com>
2022-11-08 11:47:40 +0900

    Etc - RotateMatrix

--- Etc/src/RotateMatrix.java
+++ Etc/src/RotateMatrix.java
@@ -23,20 +23,44 @@ public class RotateMatrix {
     }
 
     public static int[][] rotateMatrix(int[][] matrix, int K) {
-        // TODO:
-        if (matrix == null) return null;
-        if (K == 0 || K % 4 == 0) return matrix;
+        // 시계 방향으로 90도 회전시킨다
+        // 여러번 회전해야할 경우를 위해 K가 0이 될때까지 재귀로 회전
+        if (matrix == null || K == 0 || K % 4 == 0) return matrix;
 
         int[][] rotatedMatrix = new int[matrix[0].length][matrix.length];
 
         for (int i = 0; i < rotatedMatrix.length; i++) {
             for (int j = 0; j < rotatedMatrix[0].length; j++) {
                 rotatedMatrix[i][j] = matrix[matrix.length - j - 1][i];
-
             }
         }
-
         return rotateMatrix(rotatedMatrix, K - 1);
     }
 
+    public int[][] reference(int[][] matrix, int K) {
+        //빈 배열일 경우
+        if (matrix == null || matrix.length == 0) return matrix;
+
+        int N = matrix.length;
+        int M = matrix[0].length;
+
+        int rotateNum = K % 4;
+        // 회전하지 않는다.
+        if (rotateNum == 0) return matrix;
+        // 홀수번 회전 시 M x N, 짝수번 회전 시 N x M
+        int[][] rotated = rotateNum % 2 == 1 ? new int[M][N] : new int[N][M];
+
+        for (int row = 0; row < rotated.length; row++) {
+            for (int col = 0; col < rotated[row].length; col++) {
+                if (rotateNum == 1) {
+                    rotated[row][col] = matrix[N - col - 1][row];
+                } else if (rotateNum == 2) {
+                    rotated[row][col] = matrix[N - row - 1][M - col - 1];
+                } else {
+                    rotated[row][col] = matrix[col][M - row - 1];
+                }
+            }
+        }
+        return rotated;
+    }
 }
