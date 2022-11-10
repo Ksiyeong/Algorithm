71b158c3b4df0791bebec6cf54609f151f47fb01
Ksiyeong <yuiop2767@naver.com>
2022-11-10 10:52:00 +0900

    Etc - 삽입정렬

--- Etc/src/InsertionSort.java
+++ Etc/src/InsertionSort.java
@@ -3,9 +3,21 @@ import java.util.Arrays;
 public class InsertionSort {
     public static void main(String[] args) {
         // 삽입 정렬
-        int[] output = insertionSort(new int[]{1, 2, 3, 4, 3, 5, 3, 6, 4});
+        int[] arr = new int[10000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * arr.length);
+        }
+
+        int[] arr2 = arr.clone();
+        Arrays.sort(arr2);
+
+        int[] output = insertionSort(arr);
 
         System.out.println(Arrays.toString(output));
+        System.out.println(Arrays.toString(arr2));
+
+        System.out.println(Arrays.equals(arr, arr2));
+
     }
 
     public static int[] insertionSort(int[] arr) {
@@ -21,7 +33,7 @@ public class InsertionSort {
                     arr[j + 1] = arr[j];
                     arr[j] = temp;
 
-                } else break; // 최적화 : 현재 비교할 수가 앞의 수보다 크다면 이미 이전 범위는 이미 정렬이 되었으므로 비교할 필요없음
+                } else break; // 현재 비교할 수가 앞의 수보다 크다면 이미 이전 범위는 이미 정렬이 되었으므로 비교할 필요없음
 
             }
         }
