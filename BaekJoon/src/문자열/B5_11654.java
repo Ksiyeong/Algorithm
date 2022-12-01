23261770b780ab28be7d3531d71f632026755ccc
Ksiyeong <yuiop2767@naver.com>
2022-12-01 17:44:02 +0900

    BaekJoon

--- BaekJoon/src/문자열/B5_11654.java
+++ BaekJoon/src/문자열/B5_11654.java
@@ -1,8 +1,8 @@
-package S6문자열;
+package 문자열;
 
 import java.util.Scanner;
 
-public class No1B5_11654 {
+public class B5_11654 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char c = sc.next().charAt(0);
