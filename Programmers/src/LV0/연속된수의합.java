a203a00776b72fe84f7a1c648a4fe2a65b51543f
Ksiyeong <yuiop2767@naver.com>
2023-02-08 18:04:55 +0900

    programmers - 연속된수의합

---
+++ Programmers/src/LV0/연속된수의합.java
@@ -0,0 +1,21 @@
+package LV0;
+
+import java.util.Arrays;
+
+public class 연속된수의합 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(3, 12)));
+        System.out.println(Arrays.toString(solution(5, 15)));
+        System.out.println(Arrays.toString(solution(4, 14)));
+        System.out.println(Arrays.toString(solution(5, 5)));
+    }
+
+    static public int[] solution(int num, int total) {
+        int[] answer = {};
+
+
+
+
+        return answer;
+    }
+}
