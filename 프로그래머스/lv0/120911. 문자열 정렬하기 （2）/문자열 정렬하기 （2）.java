f9da0dcd4c64579b164737ca194275f9955de09b
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-02-09 16:10:35 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 3.23 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string) {
+        return Arrays.stream(my_string.toLowerCase().split(""))
+                .sorted()
+                .collect(Collectors.toList())
+                .toString()
+                .replaceAll("\\W", "");
+    }
+}
