1ad43f3b2a743eaabdba783cae03cc956d053bd6
Ksiyeong <yuiop2767@naver.com>
2023-02-09 14:33:51 +0900

    [programmer] 연속된수의합

--- Programmers/src/LV0/연속된수의합.java
+++ Programmers/src/LV0/연속된수의합.java
@@ -11,10 +11,21 @@ public class 연속된수의합 {
     }
 
     static public int[] solution(int num, int total) {
-        int[] answer = {};
+        int[] answer = new int[num];
 
+        int a = total / num;
 
+        int b = num / 2;
 
+        int c = a - b;
+
+        for (int i = 0; i < num; i++) {
+            if (num % 2 == 0) {
+                answer[i] = c + i + 1;
+            } else {
+                answer[i] = c + i;
+            }
+        }
 
         return answer;
     }
