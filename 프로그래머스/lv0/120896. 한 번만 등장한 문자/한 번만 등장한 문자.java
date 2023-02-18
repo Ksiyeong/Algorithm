06853cc5d924cfcd82c1230171cc503c2122b199
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-02-18 18:17:19 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 7.69 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -1,17 +1,7 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-
-        for (char i = 97; i <= 122; i++) {
-
-            int count = s.length() - s.replaceAll(i + "", "").length();
-
-            if (count == 1) {
-                answer += i;
-            }
-
-        }
-
+        for (char i = 97; i <= 122; i++) answer += s.length() - s.replaceAll(i + "", "").length() == 1 ? i : "";
         return answer;
     }
 }
