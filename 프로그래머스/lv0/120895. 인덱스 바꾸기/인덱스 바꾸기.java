a20a90025485c6f2268fcb95f6a707bf4f2d9859
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-02-19 22:53:15 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 1.85 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+
+        String[] split = my_string.split("");
+        String temp = split[num1];
+        split[num1] = split[num2];
+        split[num2] = temp;
+
+        for (String str : split) {
+            answer += str;
+        }
+        
+        return answer;
+    }
+}
