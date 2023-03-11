658267d2dc795583a3f47565f8fe78a6c6dd7fb7
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-03-11 21:26:53 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -1,14 +1,5 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[] sides) {
-        Arrays.sort(sides);
-        int answer = 0;
-        int i = sides[0] + sides[1] - 1;
-        while (i + sides[0] > sides[1]) {
-            i--;
-            answer++;
-        }
-        return answer;
+        return Math.min(sides[0], sides[1]) * 2 - 1;
     }
 }
