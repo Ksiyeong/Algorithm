51775b7241f8f34a31dddaed94eb27f9956ea7f7
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-03-17 15:27:10 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 2.20 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
@@ -1,27 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[][] dots) {
-        int max = dots[0][0];
-        int min = dots[0][0];
-        for (int[] dot : dots) {
-            if (dot[0] > max) {
-                max = dot[0];
-            } else if (dot[0] < min) {
-                min = dot[0];
-            }
-        }
-        int width = max - min;
-
-        max = dots[0][1];
-        min = dots[0][1];
-        for (int[] dot : dots) {
-            if (dot[1] > max) {
-                max = dot[1];
-            } else if (dot[1] < min) {
-                min = dot[1];
-            }
-        }
-        int height = max - min;
-
-        return width * height;
+        return (Arrays.stream(dots).mapToInt(dot -> dot[0]).max().getAsInt() - Arrays.stream(dots).mapToInt(dot1 -> dot1[0]).min().getAsInt()) * (Arrays.stream(dots).mapToInt(dot2 -> dot2[1]).max().getAsInt() - Arrays.stream(dots).mapToInt(dot3 -> dot3[1]).min().getAsInt());
     }
 }
