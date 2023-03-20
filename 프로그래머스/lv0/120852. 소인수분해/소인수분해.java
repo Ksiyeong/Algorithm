3ade6a4342bf73fe636190d2b17f840562f87b4c
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-03-20 17:03:25 +0900

    [level 0] Title: 소인수분해, Time: 2.09 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+
+        int i = 2;
+        while (n >= i) {
+            if (n % i == 0) {
+                answer.add(i);
+                n /= i;
+                i = 2;
+            } else {
+                i++;
+            }
+        }
+
+        return answer.stream().distinct().mapToInt(num -> num).toArray();
+    }
+}
