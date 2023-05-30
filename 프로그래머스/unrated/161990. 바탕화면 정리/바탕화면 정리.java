a0df72c5bceab72a7765ab931a7712600efbafe3
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-05-30 15:46:02 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.10 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,27 +1,14 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
         int[] answer = new int[]{wallpaper.length, wallpaper[0].length(), 0, 0};
-
         for (int i = 0; i < wallpaper.length; i++) {
-            int firstX = wallpaper[i].indexOf('#');
-            int lastX = wallpaper[i].lastIndexOf('#');
-
-            if (firstX > -1) {
-                if (answer[0] > i) {
-                    answer[0] = i;
-                }
-                if (answer[1] > firstX) {
-                    answer[1] = firstX;
-                }
-                if (answer[2] < i + 1) {
-                    answer[2] = i + 1;
-                }
-                if (answer[3] < lastX + 1) {
-                    answer[3] = lastX + 1;
-                }
+            if (wallpaper[i].contains("#")) {
+                answer[0] = Math.min(answer[0], i);
+                answer[1] = Math.min(answer[1], wallpaper[i].indexOf('#'));
+                answer[2] = Math.max(answer[2], i + 1);
+                answer[3] = Math.max(answer[3], wallpaper[i].lastIndexOf('#') + 1);
             }
         }
-        
         return answer;
     }
 }
