0d60ef8d9f0421d8d43f0bf8a0d345e1e6be457c
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-06-04 23:21:27 +0900

    [unrated] Title: 둘만의 암호, Time: 1.89 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -1,15 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
 class Solution {
-    public String solution(String s, String skip, int index) {
-        char[] sArr = s.toCharArray();
 
-        for (int i = 0; i < sArr.length; i++) {
-            for (int j = 0; j < index; j++) {
-                sArr[i] += sArr[i] >= 122 ? -25 : 1;
-                if (skip.contains(sArr[i] + "")) {
-                    j -= 1;
-                }
+
+public String solution(String s, String skip, int index) {
+    char[] sArr = s.toCharArray();
+    StringBuilder sb = new StringBuilder();
+    Set<Character> skipSet = new HashSet<>();
+    final int ALPHABET_MAX = 122;
+    final int ALPHABET_MIN = 97;
+
+    for (char c : skip.toCharArray()) {
+        skipSet.add(c);
+    }
+
+    for (int i = 0; i < sArr.length; i++) {
+        char current = sArr[i];
+        for (int j = 0; j < index; j++) {
+            current += current >= ALPHABET_MAX ? ALPHABET_MIN - ALPHABET_MAX : 1;
+            if (skipSet.contains(current)) {
+                j -= 1;
             }
         }
-        return String.valueOf(sArr);
+        sb.append(current);
     }
+
+    return sb.toString();
+}
+
 }
