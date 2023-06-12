f3227a517256a6944dcc9731251860cc12a245eb
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-06-12 15:35:39 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.48 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            int count = calculateDivisorsCount(i);
+            answer += count > limit ? power : count;
+        }
+        return answer;
+    }
+
+    public static int calculateDivisorsCount(int number) {
+        int count = 0;
+        for (int i = 1; i * i <= number; i++) {
+            if (i * i == number) {
+                count++;
+            } else if (number % i == 0) {
+                count += 2;
+            }
+        }
+        return count;
+    }
+}
