a4ec8a193da5b121532c13a38bb21967c6e24c79
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-07-13 13:11:24 +0900

    [level 1] Title: 모의고사, Time: 0.85 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,38 +1,29 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
-    public int[] solution(int[] answers) {
-        int[] numbers = new int[3];
+    public static int[] solution(int[] answers) {
+        int[][] patterns = {
+                {1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
 
-        Queue<Integer> aQ = new ArrayDeque<>(List.of(1, 2, 3, 4, 5));
-        Queue<Integer> bQ = new ArrayDeque<>(List.of(2, 1, 2, 3, 2, 4, 2, 5));
-        Queue<Integer> cQ = new ArrayDeque<>(List.of(3, 3, 1, 1, 2, 2, 4, 4, 5, 5));
-
-        for (int answer : answers) {
-            if (answer == aQ.peek()) numbers[0]++;
-            if (answer == bQ.peek()) numbers[1]++;
-            if (answer == cQ.peek()) numbers[2]++;
-            
-            aQ.add(aQ.poll());
-            bQ.add(bQ.poll());
-            cQ.add(cQ.poll());
-        }
-
-        int max = 0;
-        List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] > max) {
-                max = numbers[i];
-                answer.clear();
-                answer.add(i + 1);
-            } else if (numbers[i] == max) {
-                answer.add(i + 1);
+        int[] hit = new int[3];
+        for(int i = 0; i < hit.length; i++) {
+            for(int j = 0; j < answers.length; j++) {
+                if(patterns[i][j % patterns[i].length] == answers[j]) hit[i]++;
             }
         }
 
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        int max = Math.max(hit[0], Math.max(hit[1], hit[2]));
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < hit.length; i++)
+            if(max == hit[i]) list.add(i + 1);
+
+        int[] answer = new int[list.size()];
+        int cnt = 0;
+        for(int num : list)
+            answer[cnt++] = num;
+        return answer;
     }
 }
