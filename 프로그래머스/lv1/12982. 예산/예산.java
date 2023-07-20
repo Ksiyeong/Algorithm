1a87cfb7bd3eb1bf80e982c1c0f65df809acc217
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-07-20 23:36:38 +0900

    [level 1] Title: 예산, Time: 0.36 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int i = 0;
+        while (i < d.length && budget >= d[i]) {
+            budget -= d[i];
+            i++;
+        }
+        return i;
+    }
+}
