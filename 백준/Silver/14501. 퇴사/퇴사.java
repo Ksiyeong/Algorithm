37844cbe5a1ad957b762b1c4fec8e23bfe753c09
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-09-02 19:09:44 +0900

    [Silver III] Title: 퇴사, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -7,26 +7,37 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(bf.readLine());
         schedule = new int[n][2];
+        dp = new int[n];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = -1;
+        }
         for (int i = 0; i < n; i++) {
             String[] split = bf.readLine().split(" ");
             schedule[i][0] = Integer.parseInt(split[0]);
             schedule[i][1] = Integer.parseInt(split[1]);
         }
-        recursive(0, 0);
-        System.out.println(MONEY);
+        System.out.println(recursive(0));
     }
 
     static int n;
     static int[][] schedule;
-    static int MONEY = -1;
+    static int[] dp;
 
-    static private void recursive(int index, int money) {
+    static private int recursive(int index) {
         if (index == n) {
-            MONEY = Math.max(MONEY, money);
-            return;
-        } else if (index < n) {
-            recursive(index + schedule[index][0], money + schedule[index][1]);
-            recursive(index + 1, money);
+            return 0;
         }
+        if (index > n) {
+            return Integer.MIN_VALUE;
+        }
+        if (dp[index] != -1) {
+            return dp[index];
+        }
+        dp[index] = Math.max(
+                recursive(index + schedule[index][0]) + schedule[index][1],
+                recursive(index + 1)
+        );
+
+        return dp[index];
     }
 }
