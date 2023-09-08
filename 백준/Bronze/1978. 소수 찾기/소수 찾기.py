836e7f5ed063937f3b14aa3262b69fff8dd0ca43
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-09-08 23:32:13 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,15 +1,15 @@
 n = int(input())
 nums = list(map(int, input().split()))
-arr = list(range(2, max(nums) + 1))
+arr = [False, False] + [True] * (max(nums) - 1)
 answer = 0
 
-for i in range(len(arr)):
-    if arr[i] != 0:
-        for j in range(i + arr[i], len(arr), i + 2):
-            arr[j] = 0
+for i in range(2, len(arr)):
+    if arr[i]:
+        for j in range(i + i, len(arr), i):
+            arr[j] = False
 
 for num in nums:
-    if num in arr:
+    if arr[num]:
         answer += 1
 
 print(answer)
