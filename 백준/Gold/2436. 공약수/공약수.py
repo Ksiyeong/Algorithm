87246fb2f705461aa2420d683d2c07a189aab240
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-09-19 22:23:09 +0900

    [Gold V] Title: 공약수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2436. 공약수/공약수.py
+++ 백준/Gold/2436. 공약수/공약수.py
@@ -1,24 +1,19 @@
 def euclidean(a: int, b: int):
-    # 큰수 a
-    # 작은 수 b
-    while a % b != 0:
-        temp = b
-        b = a % b
-        a = temp
-    
-    return b
+    while b:
+        a, b = b, a % b
+    return a
+
+def solution(gcd: int, lcm: int):
+    ab = gcd * lcm
+    A = gcd
+    B = lcm
+    for a in range(gcd, int(ab ** 0.5) + 1, gcd):
+        if ab % a == 0:
+            b = ab // a
+            if euclidean(a, b) == gcd:
+                if A + B > a + b:
+                    A, B = a, b
+    print(A, B)
 
 gcd, lcm = map(int, input().split())
-ab = gcd * lcm
-A = gcd
-B = lcm
-
-for a in range(gcd, int(ab ** 0.5) + 1, gcd):
-    if ab % a == 0:
-        b = ab // a
-        if euclidean(a, b) == gcd:
-            if A + B > a + b:
-                A = a
-                B = b
-
-print(A, B)
+solution(gcd, lcm)
