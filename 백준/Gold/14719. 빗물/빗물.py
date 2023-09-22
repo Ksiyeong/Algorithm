95a2e70722ee02226080f1cbf5d829ad4f5ab757
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-09-22 15:49:11 +0900

    [Gold V] Title: 빗물, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,9 +1,9 @@
 def solution(w: int, blocks: list):
     answer = 0
     highest = max(blocks)
-    now_highest = 0
 
     # 정방향
+    now_highest = 0
     stop_index1 = 0
     for i in range(w):
         if blocks[i] >= highest:
@@ -15,8 +15,8 @@ def solution(w: int, blocks: list):
             now_highest = blocks[i]
 
     # 역방향
-    stop_index2 = 0
     now_highest = 0
+    stop_index2 = 0
     for i in range(w - 1, -1, -1):
         if blocks[i] >= highest:
             stop_index2 = i
