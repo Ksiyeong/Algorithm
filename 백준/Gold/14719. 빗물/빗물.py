99ada127b6164931b6e7b8cb21b3b826797b2368
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-09-22 16:08:08 +0900

    [Gold V] Title: 빗물, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,37 +1,14 @@
-def solution(w: int, blocks: list):
-    answer = 0
-    highest = max(blocks)
-
-    # 정방향
-    now_highest = 0
-    stop_index1 = 0
-    for i in range(w):
-        if blocks[i] >= highest:
-            stop_index1 = i
-            break
-        elif blocks[i] < now_highest:
-            answer += now_highest - blocks[i]
-        else: # blocks[i] > now_highest:
-            now_highest = blocks[i]
-
-    # 역방향
-    now_highest = 0
-    stop_index2 = 0
-    for i in range(w - 1, -1, -1):
-        if blocks[i] >= highest:
-            stop_index2 = i
-            break
-        elif blocks[i] < now_highest:
-            answer += now_highest - blocks[i]
-        else:
-            now_highest = blocks[i]
-
-    # 중간 부분 계산
-    for i in range(stop_index1 + 1, stop_index2):
-        answer += highest - blocks[i]
-
-    print(answer)
-
 h, w = map(int, input().split())
-blocks = list(map(int, input().split()))
-solution(w, blocks)
+world = list(map(int, input().split()))
+
+answer = 0
+for i in range(1, w - 1):
+    left_max = max(world[:i])
+    right_max = max(world[i+1:])
+
+    compare = min(left_max, right_max)
+
+    if world[i] < compare:
+        answer += compare - world[i]
+
+print(answer)
