7218c99a06327e79b6add019858c71816de13d6f
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-09-28 18:50:21 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] nums;
+    static int[] arr;
+    static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        nums = new int[N];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        br.close();
+        Arrays.sort(nums);
+
+        arr = new int[M];
+        visit = new boolean[N];
+
+        solution(0, 0);
+
+        System.out.println(sb);
+    }
+
+    static void solution(int start, int depth) {
+        if (depth == M) {
+            for (int i : arr) {
+                sb.append(i).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = nums[i];
+                solution(i + 1, depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
