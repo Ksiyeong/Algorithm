221ecca9fda073408b1a1d18c0e613cb66bc9d00
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-10-10 13:55:38 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,19 +1,22 @@
 from sys import stdin
 input = stdin.readline
 
-def solution(schedule:list):
-    n = len(schedule)
-    dp = [0] * (n+1)
+def solution(index:int):
+    if index == N:
+        return 0 
+    if index > N:
+        return -1e9
+    if dp[index] != -1:
+        return dp[index]
 
-    for i in range(n-1, -1, -1):
-        if i + schedule[i][0] > n:
-            dp[i] = dp[i+1]
-        else:    
-            dp[i] += max(dp[i+1], schedule[i][1]+dp[i+schedule[i][0]])
-
-    return dp[0]
+    dp[index] = max(
+        solution(index+schedule[index][0]) + schedule[index][1],
+        solution(index+1)
+    )
+    return dp[index]
 
 N = int(input())
 schedule = [list(map(int, input().split())) for _ in range(N)]
+dp = [-1] * N
 
-print(solution(schedule))
+print(solution(0))
