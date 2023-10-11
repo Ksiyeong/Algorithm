09cb22f1017f4816edaeae8508fc66fcdfbb43a3
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-10-11 20:18:28 +0900

    [Gold V] Title: 평범한 배낭, Time: 192 ms, Memory: 53720 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -6,44 +6,38 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         String[] split = bf.readLine().split(" ");
-        n = Integer.parseInt(split[0]);
-        k = Integer.parseInt(split[1]);
-        dp = new int[n][k + 1];
-        for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < dp[i].length; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        items = new int[n][2];
+        int n = Integer.parseInt(split[0]);
+        int k = Integer.parseInt(split[1]);
+
+        int[][] items = new int[n][2];
         for (int i = 0; i < n; i++) {
             split = bf.readLine().split(" ");
             items[i][0] = Integer.parseInt(split[0]);
             items[i][1] = Integer.parseInt(split[1]);
         }
 
-        System.out.println(recursive(0, 0));
+        System.out.println(solution(n, k, items));
     }
 
-    static int n;
-    private static int k;
-    static int[][] items;
-    static int[][] dp;
 
-    static private int recursive(int index, int weight) {
-        if (weight > k) {
-            return Integer.MIN_VALUE;
-        }
-        if (index == n) {
-            return 0;
-        }
-        if (dp[index][weight] != -1) {
-            return dp[index][weight];
-        }
-        dp[index][weight] = Math.max(
-                recursive(index + 1, weight + items[index][0]) + items[index][1],
-                recursive(index + 1, weight)
-        );
+    static private int solution(int n, int k, int[][] items) {
+        int[][] dp = new int[n + 1][k + 1];
 
-        return dp[index][weight];
+        for (int index = 1; index <= n; index++) {
+            for (int weight = 1; weight <= k; weight++) {
+
+                if (weight < items[index - 1][0]) {
+                    dp[index][weight] = dp[index - 1][weight];
+                } else {
+                    dp[index][weight] = Math.max(
+                            dp[index - 1][weight],
+                            dp[index - 1][weight - items[index - 1][0]] + items[index - 1][1]
+                    );
+                }
+
+            }
+        }
+
+        return dp[n][k];
     }
 }
