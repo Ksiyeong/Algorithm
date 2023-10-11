f8c0039e39e207d03eece75f161366d254ae1329
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-10-11 17:54:21 +0900

    [Gold V] Title: 평범한 배낭, Time: 1876 ms, Memory: 156620 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,9 +1,12 @@
 from sys import stdin
 input = stdin.readline
 
-def solution(idx, w):
+N, K = map(int, input().split())
+items = [list(map(int, input().split())) for _ in range(N)]
+
+def solution(idx:int, w:int):
     if w > K:
-        return - 1e9
+        return -1e9
     if idx == N:
         return 0
     if dp[idx][w] != -1:
@@ -12,11 +15,10 @@ def solution(idx, w):
     dp[idx][w] = max(
         solution(idx+1, w+items[idx][0]) + items[idx][1],
         solution(idx+1, w)
-    )
+        )
+    
     return dp[idx][w]
 
-N, K = map(int, input().split())
-items = [list(map(int, input().split())) for _ in range(N)]
 dp = [[-1] * (K+1) for _ in range(N)]
 
-print(solution(0, 0))
+print(solution(0,0))
