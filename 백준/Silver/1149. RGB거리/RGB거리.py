d380fc006419c2851cedab80d989bf5c108a5f02
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-10-13 14:24:55 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -4,13 +4,12 @@ input = stdin.readline
 N = int(input())
 rgb = [list(map(int, input().split())) for _ in range(N)]
 
-dp = [[0] * 3 for _ in range(N+1)]
-for idx in range(1, N+1):
+for idx in range(1, N):
     # RED 0
-    dp[idx][0] = rgb[idx-1][0] + min(dp[idx-1][1], dp[idx-1][2])
+    rgb[idx][0] += min(rgb[idx-1][1], rgb[idx-1][2])
     # GREEN 1
-    dp[idx][1] = rgb[idx-1][1] + min(dp[idx-1][0], dp[idx-1][2])
+    rgb[idx][1] += min(rgb[idx-1][0], rgb[idx-1][2])
     # BLUE 2
-    dp[idx][2] = rgb[idx-1][2] + min(dp[idx-1][0], dp[idx-1][1])
+    rgb[idx][2] += min(rgb[idx-1][0], rgb[idx-1][1])
 
-print(min(dp[N]))
+print(min(rgb[N-1]))
