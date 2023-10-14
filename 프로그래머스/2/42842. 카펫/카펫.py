94cfea542a103fe900eb0431de57e18b347ad394
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-10-14 22:09:43 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,10 +1,3 @@
-def solution(brown:int, yellow:int):
-    # 사각형의 넓이를 이용하여 가로, 세로 길이를 구하라.
-    area = brown + yellow
-
-    # 최소 3
-    for width in range(3, 100000000):
-        if area % width == 0:
-            length = int(area / width)
-            if (width + length) * 2 - 4 == brown:
-                return [max(width, length), min(width, length)]
+def solution(brown, yellow):
+    w = int(((brown+4)/2 + (((brown+4)/2)**2 - 4*(brown+yellow))**0.5) / 2)
+    return [w, ((brown+yellow)//w)]
