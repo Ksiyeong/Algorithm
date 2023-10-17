a07c2fe832ece51a8ce11ae6577126a16dd86830
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-10-17 17:06:47 +0900

    [Gold III] Title: 내리막 길, Time: 168 ms, Memory: 18840 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -5,6 +5,7 @@ public class Main {
 
         road = new int[M][N];
         dp = new int[M][N];
+
         for (int y = 0; y < M; y++) {
             for (int x = 0; x < N; x++) {
                 road[y][x] = read();
@@ -12,7 +13,8 @@ public class Main {
             }
         }
 
-        System.out.print(solution(0, 0));
+        solution(0, 0);
+        System.out.print(dp[0][0]);
     }
 
     static int M;
@@ -20,31 +22,28 @@ public class Main {
     static int[][] road;
     static int[][] dp;
 
-    static private int solution(int y, int x) {
+    private static void solution(int y, int x) {
         if (y == M - 1 && x == N - 1) {
-            return 1;
+            dp[y][x] = 1;
+            return;
         }
 
         if (dp[y][x] != -1) {
-            return dp[y][x];
+            return;
         }
 
         dp[y][x] = 0;
-        // 상, 하, 좌, 우
         for (int[] temp : new int[][]{{y - 1, x}, {y + 1, x}, {y, x - 1}, {y, x + 1}}) {
             int dy = temp[0];
             int dx = temp[1];
 
-            // 범위 제한
             if (0 <= dy && dy < M && 0 <= dx && dx < N) {
-                // 경사도 검사
                 if (road[y][x] > road[dy][dx]) {
-                    dp[y][x] += solution(dy, dx);
+                    solution(dy, dx);
+                    dp[y][x] += dp[dy][dx];
                 }
             }
         }
-
-        return dp[y][x];
     }
 
     private static int read() throws Exception {
