794a8db71ea8d834f0801a56302f501acca6e45a
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-10-17 16:27:43 +0900

    [Gold III] Title: 내리막 길, Time: 140 ms, Memory: 42764 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -5,23 +5,25 @@ input = stdin.readline
 def solution(y, x):    
     # 목적지 도착 여부
     if y == M-1 and x == N-1:
-        return 1
+        dp[y][x] = 1
+        return
     
     # 이미 방문한곳
     if dp[y][x] != -1:
-        return dp[y][x]
+        return
+    
+    # 방문 표시
+    dp[y][x] = 0
     
     # 상, 하, 좌, 우
-    route = 0
     for dy, dx in [[y-1, x], [y+1, x], [y, x-1], [y, x+1]]:
         if 0 <= dy < M and 0 <= dx < N:
             if road[y][x] > road[dy][dx]:
-                route += solution(dy, dx)
-
-    dp[y][x] = route
-    return dp[y][x]
+                solution(dy, dx)
+                dp[y][x] += dp[dy][dx]
 
 M, N = map(int, input().split())
 road = [list(map(int, input().split())) for _ in range(M)]
 dp = [[-1] * N for _ in range(M)]
-print(solution(0, 0))
+solution(0, 0)
+print(dp[0][0])
