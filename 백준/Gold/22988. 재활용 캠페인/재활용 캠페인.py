da620fa85b11bff390abbb3dc58fe5fccfbef6bc
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-10-22 19:21:03 +0900

    [Gold II] Title: 재활용 캠페인, Time: 148 ms, Memory: 45108 KB -BaekjoonHub

--- 백준/Gold/22988. 재활용 캠페인/재활용 캠페인.py
+++ 백준/Gold/22988. 재활용 캠페인/재활용 캠페인.py
@@ -1,34 +1,31 @@
-N, X = map(int,input().split())
+from sys import stdin
+input = stdin.readline
 
-arr = sorted(list(map(int,input().split())))
+n, x = map(int, input().split())
+arr = sorted(list(map(int, input().split())))
 
-
-s = 0
-
-e = N-1
-
+answer = 0
 remain = 0
-cnt = 0
-while s <= e : # s와 e가 교차되면 멈춘다!
+target = x / 2
 
-    if arr[e] == X:
-        cnt += 1
-        e -= 1
+i = 0
+j = n - 1
+while i <= j:
+    if arr[j] == x:
+        answer += 1
+        j -= 1
         continue
 
-    if s == e :
-        remain += 1 
-        break
-        # 짜투리를 하나 추가한다!
-
-    if arr[e] + arr[s] >= X/2:
-        cnt +=1
-        s += 1
-        e -= 1
-
-    else:
-        s += 1 # 수가 커지겠죠!
+    if i == j:
         remain += 1
+        break
 
+    if arr[i] + arr[j] >= target:
+        answer += 1
+        i += 1
+        j -= 1
+    else:
+        remain += 1
+        i += 1
 
-print(cnt + remain//3 )
+print(answer + remain//3)
