eb86400f03743d3cf66b0893c460e26e3cba8daf
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-10-22 22:10:45 +0900

    [Gold II] Title: 재활용 캠페인, Time: 148 ms, Memory: 44084 KB -BaekjoonHub

--- 백준/Gold/22988. 재활용 캠페인/재활용 캠페인.py
+++ 백준/Gold/22988. 재활용 캠페인/재활용 캠페인.py
@@ -2,24 +2,21 @@ from sys import stdin
 input = stdin.readline
 
 n, x = map(int, input().split())
-arr = sorted(list(map(int, input().split())))
+arr = sorted(list(filter(lambda a: a < x, map(int, input().split()))))
 
-answer = 0
+answer = n - len(arr)
+n -= answer
+
+target = x /2
 
 i = 0
 j = n - 1
-while i <= j:
-    if arr[j] == x:
+while i < j:
+    if arr[i] + arr[j] >= target:
         answer += 1
-        n -= 1
-        j -= 1
-    elif i == j:
-        break
-    elif (arr[i] + arr[j]) * 2 >= x:
-        answer += 1
-        n -= 2
         i += 1
         j -= 1
+        n -= 2
     else:
         i += 1
 
