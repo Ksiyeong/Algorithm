c1a99736a904e43f1b7fd9c13a6a7f6ede2e75d6
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-10-23 15:25:33 +0900

    [Gold IV] Title: 고냥이, Time: 476 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.py
+++ 백준/Gold/16472. 고냥이/고냥이.py
@@ -3,24 +3,27 @@ input = stdin.readline
 
 n = int(input())
 arr = input().rstrip()
+# arr = list(input().rstrip())
 
 answer = 0
 use = set()
 i = 0
 j = 0
 while j < len(arr):
-    # 포함되어 있지 않고 글자를 추가할 수 있다면
-    if arr[j] not in use and len(use) < n:
-        use.add(arr[j])
-        j += 1
-    # 포함되어 있지 않고 글자를 추가할 수 없다면 == 가득찼는데, 포함은 안됨. 초기화할 타이밍
-    elif arr[j] not in use and len(use) >= n:
-        answer = max(answer, j-i)
-        i += 1
-        j = i
-        use.clear()
+    # 포함되어 있지 않고
+    if arr[j] not in use:
+        # 글자를 추가할 수 있다면
+        if len(use) < n:
+            use.add(arr[j])
+            j += 1
+        # 글자를 추가할 수 없다면
+        else:
+            answer = max(answer, j-i)
+            i += 1
+            j = i
+            use.clear()
     # 포함되어 있다면
-    elif arr[j] in use:
+    else:
         j += 1
 
 print(max(answer, j-i))
