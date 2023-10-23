3d96b01304b4e2f1fd9cc765e223bcaec925799d
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-10-23 17:05:12 +0900

    [Gold IV] Title: 고냥이, Time: 156 ms, Memory: 15672 KB -BaekjoonHub

---
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(solution(read(), new BufferedReader(new InputStreamReader(System.in)).readLine().toCharArray()));
+    }
+
+    private static int solution(int n, char[] arr) {
+        int[] alphabet = new int[26];
+        int answer = 0;
+        int count = 0;
+        int i = 0;
+        int j = 0;
+        while (j < arr.length) {
+            if (alphabet[arr[j] - 'a'] == 0) { // 새로운 알파벳일 경우
+                count += 1;
+            }
+            alphabet[arr[j] - 'a'] += 1;
+
+            while (count > n && i < j) { // count가 n보다 클경우 줄어들때까지 i위치를 이동
+                alphabet[arr[i] - 'a'] -= 1;
+                if (alphabet[arr[i] - 'a'] == 0) { // 사용중인 알파벳 하나를 없앴을 경우
+                    count -= 1;
+                }
+                i += 1;
+            }
+
+            answer = Math.max(answer, j - i + 1);
+            j += 1;
+        }
+
+        return answer;
+    }
+
+    // 읽기
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+}
