1ad35819dc5bf64eba5d674ae68409f67712c73e
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-10-29 13:43:49 +0900

    [Gold V] Title: 보물섬, Time: 580 ms, Memory: 295592 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.java
+++ 백준/Gold/2589. 보물섬/보물섬.java
@@ -1,15 +1,14 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int Y = read();
-        int X = read();
-        boolean[][] map = new boolean[Y][X];
+        Y = read();
+        X = read();
+        map = new boolean[Y][X];
         for (int y = 0; y < Y; y++) {
             char[] temp = br.readLine().toCharArray();
             for (int x = 0; x < X; x++) {
@@ -23,34 +22,45 @@ public class Main {
 
         for (int y = 0; y < Y; y++) {
             for (int x = 0; x < X; x++) {
-                if (map[y][x]) {
-                    boolean[][] visited = new boolean[Y][X]; // 방문 여부
-                    int[][] dist = new int[Y][X]; // 위치별 거리
+                answer = Math.max(answer, solution(y, x));
+            }
+        }
 
-                    // BFS
-                    Queue<int[]> q = new ArrayDeque<>();
-                    q.add(new int[]{y, x});
-                    visited[y][x] = true;
-                    while (!q.isEmpty()) {
-                        int[] yx = q.poll();
-                        for (int[] direction : new int[][]{{1, 0}, {-1, 0}, {0, -1}, {0, 1}}) {
-                            int dy = yx[0] + direction[0];
-                            int dx = yx[1] + direction[1];
-                            if (0 <= dy && dy < Y && 0 <= dx && dx < X) { // 범위 확인
-                                if (map[dy][dx] && !visited[dy][dx]) { // 길 && 방문 여부
-                                    visited[dy][dx] = true; // 방문 표시
-                                    dist[dy][dx] = dist[yx[0]][yx[1]] + 1; // 거리 표시
-                                    answer = Math.max(answer, dist[dy][dx]); // 정답 비교
-                                    q.add(new int[]{dy, dx}); // 다음 출발지 표시
-                                }
-                            }
+        System.out.println(answer);
+    }
+
+    static int Y;
+    static int X;
+    static boolean[][] map;
+    static int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+
+    private static int solution(int y, int x) {
+        int answer = 0;
+        if (map[y][x]) {
+            boolean[][] visited = new boolean[Y][X]; // 방문 여부
+            int[][] dist = new int[Y][X]; // 위치별 거리
+
+            // BFS
+            Queue<int[]> q = new LinkedList<>();
+            q.offer(new int[]{y, x});
+            visited[y][x] = true;
+            while (!q.isEmpty()) {
+                int[] yx = q.poll();
+                for (int[] direction : directions) {
+                    int dy = yx[0] + direction[0];
+                    int dx = yx[1] + direction[1];
+                    if (0 <= dy && dy < Y && 0 <= dx && dx < X) { // 범위 확인
+                        if (map[dy][dx] && !visited[dy][dx]) { // 길 && 방문 여부
+                            visited[dy][dx] = true; // 방문 표시
+                            dist[dy][dx] = dist[yx[0]][yx[1]] + 1; // 거리 표시
+                            answer = Math.max(answer, dist[dy][dx]); // 정답 비교
+                            q.offer(new int[]{dy, dx}); // 다음 출발지 표시
                         }
                     }
                 }
             }
         }
-
-        System.out.println(answer);
+        return answer;
     }
 
     // 읽기
