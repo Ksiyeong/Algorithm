2945a8f430be844e4afb97d50262ff4d337632c4
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-10-31 23:11:20 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.11 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,15 +1,5 @@
 def solution(n, words):
-    answer = [1, 1]
-    used = [words.pop(0)]
-
-    for word in words:
-        answer[0] += 1
-        if answer[0] > n:
-            answer[0] = 1
-            answer[1] += 1
-        # 끝말잇기 확인 or 사용 여부 확인
-        if used[-1][-1] != word[0] or word in used:
-            return answer
-        used.append(word)
-
+    for i in range(1, len(words)):
+        if words[i-1][-1] != words[i][0] or words[i] in words[:i]:
+            return [i%n+1, i//n+1]
     return [0, 0]
