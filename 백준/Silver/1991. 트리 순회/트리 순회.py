27365ec3962ffd508d4cf06d7caf921492cf52f5
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-11-01 16:09:25 +0900

    [Silver I] Title: 트리 순회, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,34 +1,30 @@
 from sys import stdin
 input = stdin.readline
 
-def solution(node:int):
-    if node < 0:
-        return
-    global preorder_traversal
-    global inorder_traversal
-    global postorder_traversal
-    
-    chr_node = chr(node + 65)
+def solution(node:str):
+    if node != '.':
+        global preorder
+        global inorder
+        global postorder
 
-    preorder_traversal += chr_node # 전위 순회
-    solution(graph[node][0])
-    inorder_traversal += chr_node # 중위 순회
-    solution(graph[node][1])
-    postorder_traversal += chr_node # 후위 순회
+        preorder += node # 전위 순회
+        solution(tree[node][0])
+        inorder += node # 중위 순회
+        solution(tree[node][1])
+        postorder += node # 후위 순회
 
 N = int(input())
-graph = [[] for _ in range(26)]
+tree = {}
 for _ in range(N):
-    a, b, c = map(lambda x: ord(x) - 65, input().split())
-    graph[a].append(b)
-    graph[a].append(c)
+    a, b, c = input().split()
+    tree[a] = [b, c]
 
-preorder_traversal = ''
-inorder_traversal = ''
-postorder_traversal = ''
+preorder = ''
+inorder = ''
+postorder = ''
 
-solution(0)
+solution('A')
 
-print(preorder_traversal)
-print(inorder_traversal)
-print(postorder_traversal)
+print(preorder)
+print(inorder)
+print(postorder)
