6f8e70f76e83e24b422d33d4cf47987fa81cf619
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-11-04 01:32:14 +0900

    [level 2] Title: 구명보트, Time: 7.10 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=python3) 
 
 ### 성능 요약
 
-메모리: 53 MB, 시간: 10.21 ms
+메모리: 10.5 MB, 시간: 7.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 6일 1:26:34
+2023년 11월 6일 1:32:12
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,5 +1,4 @@
 def solution(people:list, limit:int):
-    answer = 0
     people.sort()
 
     s = 0
@@ -7,13 +6,6 @@ def solution(people:list, limit:int):
     while s < e:
         if limit >= people[s] + people[e]:
             s += 1
-            e -= 1
-            answer += 1
-        else:
-            e -= 1
-            answer += 1
-        
-    if s == e:
-        answer += 1
-        
-    return answer
+        e -= 1
+
+    return len(people) - s
