fcfea98cee55f72947ee2dcec5650d669e40e6a8
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-11-24 17:38:05 +0900

    [level 2] Title: 튜플, Time: 0.14 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,21 +1,24 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class Solution {
     public int[] solution(String s) {
         Map<String, Integer> map = new HashMap<>();
 
-        String[] tuples = s.substring(2, s.length() - 2).split("},\\{");
-        for (String tuple : tuples) {
-            String[] nums = tuple.split(",");
-            for (String key : nums) {
-                map.put(key, map.getOrDefault(key, 0) + 1);
-            }
+        // 숫자 추출
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(s);
+        while (matcher.find()) {
+            String num = matcher.group();
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        int[] answer = new int[tuples.length];
+        // 정답 추출
+        int[] answer = new int[map.size()];
         for (String key : map.keySet()) {
-            answer[tuples.length - map.get(key)] = Integer.parseInt(key);
+            answer[answer.length - map.get(key)] = Integer.parseInt(key);
         }
 
         return answer;
