8ad343cbfe93ef57db2fea3f328905d15f9fe250
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-11-26 16:23:17 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,42 +1,25 @@
 class Solution {
-    public int solution(int k, int[][] dungeons) {
-        this.k = k;
-        this.dungeons = dungeons;
-        visited = new boolean[dungeons.length];
-        sequence = new int[dungeons.length];
-        permutation(0);
-        return answer;
-    }
-
-    static int k;
     static int answer = 0;
     static int[][] dungeons;
     static boolean[] visited;
-    static int[] sequence;
+    
+    public int solution(int k, int[][] dungeons) {
+        this.dungeons = dungeons;
+        visited = new boolean[dungeons.length];
+        permutation(k, 0);
+        return answer;
+    }
 
-    public void permutation(int depth) {
-        if (depth == dungeons.length) {
-            int energy = k;
-            int count = 0;
-            for (int i : sequence) {
-                if (energy >= dungeons[i][0]) {
-                    energy -= dungeons[i][1];
-                    count += 1;
-                }
-            }
-            if (answer < count) {
-                answer = count;
-            }
-            return;
-        }
-
+    public void permutation(int energy, int count) {
         for (int i = 0; i < dungeons.length; i++) {
-            if (!visited[i]) {
+            if (!visited[i] && dungeons[i][0] <= energy) {
                 visited[i] = true;
-                sequence[depth] = i;
-                permutation(depth + 1);
+                permutation(energy - dungeons[i][1], count + 1);
                 visited[i] = false;
             }
+        }
+        if (answer < count) {
+            answer = count;
         }
     }
 }
