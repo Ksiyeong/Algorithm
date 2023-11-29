791eaac0424d9d6d434b00a1ed08b4ed2ee3bfd1
Ksiyeong <107738517+Ksiyeong@users.noreply.github.com>
2023-11-29 16:40:54 +0900

    [level 2] Title: 전화번호 목록, Time: 86.89 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,30 +1,25 @@
 import java.util.HashMap;
 import java.util.Map;
 
-
 class Solution {
-    public boolean solution(String[] phoneBook) {
-        boolean answer = true;
+    public boolean solution(String[] phone_book) {
+        Map<String, Boolean> map = new HashMap<>();
+        boolean[] lengthArr = new boolean[21];
+        for (String phone : phone_book) {
+            lengthArr[phone.length()] = true;
+            map.put(phone, true);
+        }
 
-            Map<String, Integer> map = new HashMap<>();
-
-            for(int i = 0; i < phoneBook.length; i++) {
-                map.put(phoneBook[i], i);
-            }
-
-
-            for(int i = 0; i < phoneBook.length; i++) {
-                for(int j = 0; j < phoneBook[i].length(); j++) {
-                    if(map.containsKey(phoneBook[i].substring(0,j))) {
-                        answer = false;
-                        return answer;
+        for (String phone : phone_book) {
+            for (int i = 1; i < phone.length(); i++) {
+                if (lengthArr[i]) {
+                    if (map.containsKey(phone.substring(0, i))) {
+                        return false;
                     }
                 }
             }
+        }
 
-
-
-
-            return answer;
+        return true;
     }
 }
